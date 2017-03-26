2cadbf319bcb02e35bf539b1f396dddedc123d71
Guillaume DEMICHELI <30379@supinfo.com>
2017-03-27 01:34:03 +0200

    Cleanup

--- SpinnerWheel/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,2 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "antistatic.spinnerwheel", Managed="Antistatic.Spinnerwheel")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "antistatic.spinnerwheel.adapters", Managed="Antistatic.Spinnerwheel.Adapters")]
